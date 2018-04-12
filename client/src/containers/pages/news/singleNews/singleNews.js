dfb9ed481adbb74b1808849f818e78f5f0d508cc
Eve Shi <qianyiwork@gmail.com>
2018-04-12 16:50:12 +0800

    update

--- client/src/containers/pages/news/singleNews/singleNews.js
+++ client/src/containers/pages/news/singleNews/singleNews.js
@@ -1,1 +1,17 @@
 import React from 'react';
+
+const singleNews = (props) => {
+    return(
+        <div>
+            <div>
+                <img src={props.titlePic} alt='title'/>
+                <p>{props.title}</p>
+                <p>{props.uploadTime}</p>
+            </div>
+            <p>{props.caption}</p>
+        </div>
+    )
+};
+
+export default singleNews;
+
