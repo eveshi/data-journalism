295895ace32b03fd23e59da73c2c1bc0c0795a6d
Eve Shi <qianyiwork@gmail.com>
2018-04-15 01:06:45 +0800

    update

--- client/src/containers/pages/newLesson/newLesson.js
+++ client/src/containers/pages/newLesson/newLesson.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import axios from '../../../axios_data';
+import marked from 'marked';
 import InputLesson from '../../../components/inputPost/inputPost';
 import Button from '../../../components/button/button';
 
@@ -16,28 +17,62 @@ class NewLesson extends Component{
                 placeholder: '',
                 value: '',
             },
-            conmment:{
+            comment:{
                 inputType: null,
                 value: [],
+            },
+            time:{
+                inputType: null,
+                value: null,
             }
         }
     }
 
-    submitHandler = () => {
-        const lesson = this.state.lesson
-        this.uploadLesson(lesson)
+    submitHandler = async() => {
+        let lesson = this.state.lesson
+        const time = Date.now()
+        lesson.time.value = time
+        let lessonWillBeSent = {}
+        for(let key in lesson){
+            let lessonElement = {}
+            lessonElement[key] = lesson[key].value
+            lessonWillBeSent = {...lessonWillBeSent,...lessonElement}
+        }
+        const request = await this.uploadLesson(lessonWillBeSent)
     }
 
     uploadLesson = async(lesson) => await axios.post('/api/sendLesson',{
         lesson: lesson
     })
 
+    changeHandler = (event,id) => {
+        let value = event.value
+    } 
+
     render(){
+        let lesson = this.state.lesson
+        for(let key in lesson){
+            lesson = {
+                id: key,
+                ...lesson
+            }
+        }
+
+        const contentValue = lesson.content.value
+        const contentHtml = marked(contentValue)
+
         return(
             <div>
                 <form>
-                    {this.state.lesson.map((el)=>{
-                        <InputLesson />
+                    {lesson.map((el)=>{
+                        return (
+                            <InputLesson
+                                key = {el.id}
+                                inputType = {el.inputType}
+                                placeholder = {el.placeholder}
+                                value = {el.value}
+                                change = {(event) => this.changeHandler(event,el.id)}
+                                inputContentDisplay = {contentHtml} />)
                     })}
                 </form>
                 <Button onClick={this.submitHandler} name='submit'/>
@@ -45,3 +80,5 @@ class NewLesson extends Component{
         )
     }
 }
+
+export default NewLesson;
