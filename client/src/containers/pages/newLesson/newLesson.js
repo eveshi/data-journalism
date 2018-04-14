0a884f18c602c0c5350f9df0ccbf2a71bf8931e4
Eve Shi <qianyiwork@gmail.com>
2018-04-14 23:22:22 +0800

    update

--- client/src/containers/pages/newLesson/newLesson.js
+++ client/src/containers/pages/newLesson/newLesson.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import InputLesson from '../../../components/inputPost/inputPost';
+import Button from '../../../components/button/button';
 
 class NewLesson extends Component{
     state = {
@@ -21,10 +23,24 @@ class NewLesson extends Component{
         }
     }
 
+    submitHandler = () => {
+        const lesson = this.state.lesson
+        this.uploadLesson(lesson)
+    }
+
+    uploadLesson = async(lesson) => await axios.post('/api/sendLesson',{
+        lesson: lesson
+    })
+
     render(){
         return(
             <div>
-                <InputLesson />
+                <form>
+                    {this.state.lesson.map((el)=>{
+                        <InputLesson />
+                    })}
+                </form>
+                <Button onClick={this.submitHandler} name='submit'/>
             </div>
         )
     }
