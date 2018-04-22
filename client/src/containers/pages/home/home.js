92773e4f8a84be1718948ee288525df04c60aa81
Eve Shi <qianyiwork@gmail.com>
2018-04-22 23:49:09 +0800

    update

---
+++ client/src/containers/pages/home/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { homedir } from 'os';
+
+class Home extends Component {
+
+    render(){
+        return(
+            
+        )
+    }
+}
+
+export default Home
