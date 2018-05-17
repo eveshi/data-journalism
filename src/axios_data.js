e9bf98417c3a2f3fa4d561b029833db0759a0a43
Eve Shi <qianyiwork@gmail.com>
2018-05-17 15:28:55 +0800

    update

--- src/axios_data.js
+++ src/axios_data.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Qs from 'qs';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://data-journalism.herokuapp.com/',
     headers: {"Content-Type": "application/x-www-form-urlencoded"},
     transformRequest: [function (data) { data = Qs.stringify(data); return data }]
 })
